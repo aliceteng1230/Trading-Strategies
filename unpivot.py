b91302c3e81162b22abab8e53251278d53067cda
fjm408 <fjm408@gmail.com>
2024-09-20 10:03:27 +0800

    Add files via upload

--- unpivot.py
+++ unpivot.py
@@ -1,16 +1,22 @@
 import pandas as pd
 #df = pd.read_excel('股票當天波動clear.xlsx')
 #df = pd.read_excel('cond1. 振幅clear.xlsx')
-df = pd.read_excel('cond2. 現股當沖比例clear.xlsx')
+#df = pd.read_excel('cond2. 現股當沖比例clear.xlsx')
+
+#df = pd.read_excel('cond3c.xlsx', sheet_name='成交金額')
+df = pd.read_excel('cond3c.xlsx', sheet_name='股票佔大盤權重')
 
 
 df2 = df.melt(id_vars=["股票代號"], var_name="stockid", value_name="Value")
 df2.sort_values(['股票代號', 'stockid'], ascending=[True, True], inplace=True)
 #df2.columns = ['日期', '股票代號', '當天波動']
-df2.columns = ['日期', '股票代號', '現股當沖比例']
+#df2.columns = ['日期', '股票代號', '成交金額']
+df2.columns = ['日期', '股票代號', '股票佔大盤權重']
+
 
 #df2.to_excel('股票當天波動unpivot.xlsx', index=False)
-df2.to_excel('cond2. 現股當沖比例unpivot.xlsx', index=False)
+#df2.to_excel('cond31.xlsx', index=False)
+df2.to_excel('cond32.xlsx', index=False)
 
 
 #dfa = pd.read_excel('cond1. 振幅clear.xlsx')
